9e296123b8e11c36b96b26b6bbf5699014706437
noureldeenn <nourbadr4646@gmail.com>
2021-12-15 07:31:35 +0200

    react global state

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ const options = [
   "Show some love to MUI",
   "Show all notification content",
   "Hide sensitive notification content",
-  "Hide all notification content",
 ];
 
 function App() {
